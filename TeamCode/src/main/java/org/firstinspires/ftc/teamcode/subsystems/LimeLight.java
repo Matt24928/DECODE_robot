9852db7162df57ffc53b23f8415be8545266745f
Gab1010010100 <gabrielgabrielpopescu@gmail.com>
2025-11-26 06:22:00 +0200

    Limelight cu pathfollower separat work in progress ...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimeLight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimeLight.java
@@ -14,57 +14,69 @@ public class LimeLight {
     public double dy;
 
 
-    public double x,y;
-    public double heading;
+        public double x,y;
+        public double heading;
 
 
 
     //ty = llResult.getTy()     tx = llResult.getTx()
-    public double HeadingPerpend(double tx){
-        double theta = follower.getPose().getHeading();
-        return (theta+Math.toRadians(tx));
-    }
-    public double[] coordonate(float tx, float ty) {
-        double alpha= 30;
-        double c=1;
-        double h=10;
-
-        double dy = h / Math.tan(Math.toRadians( ty + alpha)) * c;//c e constanta pt cm->inchi daca folosim cm pentru h
-        double dx = -Math.tan(Math.toRadians(tx)) * dy;
-        double theta = follower.getPose().getHeading();
-        double x = follower.getPose().getX() + dy * Math.cos(theta) - dx * Math.sin(theta);
-        double y = follower.getPose().getY() + dy * Math.sin(theta) + dx * Math.cos(theta);
-        return new double[] {x,y};
-    }//
-        public double[] distante (List <double[]>dist){
-        double[] result =new double[dist.size()];
-        double cx=23;
-        double cy=22;//set de coordonate punct de unde avem voie sa aruncam
-        int sz = dist.size();
-
-        for (int i=0;sz==i;i++) {
-            x=dist.get(i)[0];
-            y=dist.get(i)[1];
-            double d=Math.sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy));
-            result[i]=d;
+        public double HeadingPerpend(double tx){
+            double theta = follower.getPose().getHeading();
+            return (theta+Math.toRadians(tx));
         }
+        public double[] coordonate(float tx, float ty) {
 
-        return result;
+            double alpha= 30;
+            double c=1;
+            double h=10;
+
+            double dy = h / Math.tan(Math.toRadians( ty + alpha)) * c;//c e constanta pt cm->inchi daca folosim cm pentru h
+            double dx = -Math.tan(Math.toRadians(tx)) * dy;
+            double theta = follower.getPose().getHeading();
+            double x = follower.getPose().getX() + dy * Math.cos(theta) - dx * Math.sin(theta);
+            double y = follower.getPose().getY() + dy * Math.sin(theta) + dx * Math.cos(theta);
+            return new double[] {x,y};
+        }//
+        public double[] distante (List <double[]>coord){
+            double[] result =new double[coord.size()];
+            double cx=23;
+            double cy=22;//set de coordonate punct de unde avem voie sa aruncam
+            int sz = coord.size();
+
+            for (int i=0;sz==i;i++) {
+                x=coord.get(i)[0];
+                y=coord.get(i)[1];
+                double d=Math.sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy));
+                result[i]=d;
+            }
+
+            return result;
         }//calculeaza distanta de al un punct pe harta la obiect
 
-        public double indice(double[] dist){
-        double c=0,ind=0,DistMin=1000000000;
-        int i;
-        int count = dist.length;
-        for (i = 0; i <count; i++)
-            {
-                if (dist[i] < DistMin)
-                {
-                    ind = DistMin;
-                }
+        public double[] coord(List<double[]> coord){
+            double[] result =new double[coord.size()];
+            double cx=23;
+            double cy=22;//set de coordonate punct de unde avem voie sa aruncam
+            int sz = coord.size();
+
+            for (int i=0;sz==i;i++) {
+                x=coord.get(i)[0];
+                y=coord.get(i)[1];
+                double d=Math.sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy));
+                result[i]=d;
             }
-        return (dist[i]);
-        }
+            double c=0,ind=0,DistMin=1000000000;
+            int i;
+            int count = result.length;
+            for (i = 0; i <count; i++)
+                {
+                    if (result[i] < DistMin)
+                    {
+                        ind = i;
+                    }
+                }
+        return (coord.get(i));
+        }//calculeaza coord celui mai apropiat obj fata de un set de coord
 
         //am nevoie de traiectorie
     //e ora 2;30 si eu deja nu mai pot...

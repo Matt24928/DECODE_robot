ed2453ea8879c35af9a40090ce0cbf4ac8415b89
Gab1010010100 <gabrielgabrielpopescu@gmail.com>
2025-11-26 03:39:49 +0200

    Limelight cu pathfollower separat work in progress ...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimeLight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimeLight.java
@@ -18,6 +18,7 @@ public class LimeLight {
     public double heading;
 
 
+
     //ty = llResult.getTy()     tx = llResult.getTx()
     public double HeadingPerpend(double tx){
         double theta = follower.getPose().getHeading();
@@ -28,31 +29,44 @@ public class LimeLight {
         double c=1;
         double h=10;
 
-        double dy = h / java.lang.Math.tan(Math.toRadians( ty + alpha)) * c;//c e constanta pt cm->inchi daca folosim cm pentru h
-        double dx = -java.lang.Math.tan(Math.toRadians(tx)) * dy;
+        double dy = h / Math.tan(Math.toRadians( ty + alpha)) * c;//c e constanta pt cm->inchi daca folosim cm pentru h
+        double dx = -Math.tan(Math.toRadians(tx)) * dy;
         double theta = follower.getPose().getHeading();
         double x = follower.getPose().getX() + dy * Math.cos(theta) - dx * Math.sin(theta);
         double y = follower.getPose().getY() + dy * Math.sin(theta) + dx * Math.cos(theta);
         return new double[] {x,y};
     }//
-        public double distanta (double x,double y){
+        public double[] distante (List <double[]>dist){
+        double[] result =new double[dist.size()];
         double cx=23;
-        double cy=22;
-        double d=Math.sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy));
-        return d;
+        double cy=22;//set de coordonate punct de unde avem voie sa aruncam
+        int sz = dist.size();
+
+        for (int i=0;sz==i;i++) {
+            x=dist.get(i)[0];
+            y=dist.get(i)[1];
+            double d=Math.sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy));
+            result[i]=d;
+        }
+
+        return result;
         }//calculeaza distanta de al un punct pe harta la obiect
-/*
-        public int indice(List<double[]> dist){
-        double i,c=0,DistMax=;
-        int count = dist.size();
+
+        public double indice(double[] dist){
+        double c=0,ind=0,DistMin=1000000000;
+        int i;
+        int count = dist.length;
         for (i = 0; i <count; i++)
             {
-                if (DistMax<)
+                if (dist[i] < DistMin)
                 {
-
+                    ind = DistMin;
                 }
             }
-        }//am nevoie de traiectorie
+        return (dist[i]);
+        }
+
+        //am nevoie de traiectorie
     //e ora 2;30 si eu deja nu mai pot...
 
 
